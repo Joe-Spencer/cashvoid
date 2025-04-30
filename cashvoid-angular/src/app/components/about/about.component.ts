cc27bb3dcf74d6e3f875ea4a3acf206ea77a76e0
Joe <31321326+Joe-Spencer@users.noreply.github.com>
2025-04-30 15:54:14 -0400

    Update the about page

--- cashvoid-angular/src/app/components/about/about.component.ts
+++ cashvoid-angular/src/app/components/about/about.component.ts
@@ -13,7 +13,7 @@ import { HeaderComponent } from '../shared/header.component';
       <app-header title="ABOUT THE VOID"></app-header>
       <div class="content">
        <p> </p>
-        <p>Joseph Spencer is a real weirdo. In 2018 he created CASHVOID.com. He isn't entirely sure why he made CASHVOID.com or why he is typing this right now.</p>
+        <p>Joseph Spencer is a software engineer and artist. In 2018 he created CASHVOID.com. He isn't entirely sure why he made CASHVOID.com or why he is typing this right now.</p>
         <p>CASHVOID.com has gone through many iterations over the years but pretty much all of them have been bad. This might be the worst version yet.</p>
         <p><a routerLink="/chat">Speak with CHATVOID to learn more</a></p>
       </div>
